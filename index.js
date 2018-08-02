a26f8e66fad7b0e1ae24c9e91558a81f0092ec35
Keith Cirkel <keithamus@users.noreply.github.com>
2018-08-02 16:52:28 +0100

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const defaultBrowsers = [
+  'last 8 Chrome versions',
+  'last 4 Firefox versions',
+  'last 3 Safari versions',
+  'last 4 Edge versions',
+  'Firefox ESR',
+  'IE 11',
+  'Firefox >= 45'
+]
+module.exports = function babelPresetGitHub({ modules = false, targets = {} }) {
+  targets = Object.assign({}, { browsers: defaultBrowsers }, targets)
+  return {
+    plugins: [
+      // ES2019
+      // Stage 4
+      require('@babel/plugin-proposal-json-strings').default,
+      require('@babel/plugin-proposal-optional-catch-binding').default,
+      // Stage 3 with good signals for Stage 4
+      // Chrome 64+, Firefox 62+, Safari TP42, Opera 51+
+      require('@babel/plugin-syntax-import-meta').default,
+      // Chrome 63+, Firefox (https://mzl.la/2LMSnOf), Safari TP25, Opera 50+
+      require('@babel/plugin-syntax-dynamic-import').default,
+
+      // Non-standard
+      require('@babel/plugin-proposal-class-properties').default,
+      require('@babel/plugin-transform-flow-strip-types').default,
+      // Custom 
+      require('babel-plugin-transform-invariant-location'),
+    ],
+    presets: [
+      // ES6/2017
+      [require('@babel/preset-env').default, { modules, targets }]
+    ],
+  };
+};
